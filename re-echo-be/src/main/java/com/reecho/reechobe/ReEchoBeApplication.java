35a25318be3742260a6eb43503cc8d382a44a99d
haily <49448330+melly8954@users.noreply.github.com>
2026-07-06 23:36:03 +0900

    chore: Spring Boot 백엔드 프로젝트 초기화

---
+++ re-echo-be/src/main/java/com/reecho/reechobe/ReEchoBeApplication.java
@@ -0,0 +1,13 @@
+package com.reecho.reechobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReEchoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReEchoBeApplication.class, args);
+    }
+
+}
